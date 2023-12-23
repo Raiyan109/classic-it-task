a245d1b17f2ce2e24809dbe350d3f5b4ece0c631
Raiyan109 <kraiyan109@gmail.com>
2023-12-23 10:24:22 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Products from './pages/Products'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Products />
     </>
   )
 }
---
+++ src/pages/Products.jsx
@@ -0,0 +1,19 @@
+
+const Products = () => {
+    return (
+        <div className="flex justify-center items-center p-10">
+            <div className="p-10">
+                <div>
+                    <h1 className="text-5xl text-white font-semibold">Coffee Items</h1>
+                    <h1 className="text-4xl text-white font-medium">For Coffee Geeks</h1>
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
