a15039f538f12674f51c314ca75fc0bc80bf9d9c
Raiyan109 <kraiyan109@gmail.com>
2023-12-24 10:05:11 +0600

    img rendered

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,57 +1,81 @@
+import { useState } from 'react';
 import cupImg from '../assets/Milk-Tea-being-poured-into-cup-Premium-vector-PNG-1-removebg-preview.png'
 import cupImg1 from '../assets/greencup.jpeg'
 import cupImg2 from '../assets/yellowcup.jpeg'
 import { FaArrowRight } from "react-icons/fa6";
 
-
+const products = [
+    {
+        id: 1,
+        image: cupImg,
+        title: "White coffee",
+        colors: ['black', 'green', 'yellow'],
+        imgClicked: {
+            'black': true,
+            'green': false,
+            'yellow': false,
+        },
+        imgLinked: {
+            'black': cupImg,
+            'green': cupImg1,
+            'yellow': cupImg2
+        }
+    },
+    {
+        id: 2,
+        image: cupImg,
+        title: "White coffee",
+        colors: ['black', 'green', 'yellow'],
+        imgClicked: {
+            'black': true,
+            'green': false,
+            'yellow': false,
+        },
+        imgLinked: {
+            'black': cupImg,
+            'green': cupImg1,
+            'yellow': cupImg2
+        }
+    },
+    {
+        id: 3,
+        image: cupImg,
+        title: "White coffee",
+        colors: ['black', 'green', 'yellow'],
+        imgClicked: {
+            'black': true,
+            'green': false,
+            'yellow': false,
+        },
+        imgLinked: {
+            'black': cupImg,
+            'green': cupImg1,
+            'yellow': cupImg2
+        }
+    },
+]
 const Products = () => {
-    const products = [
-        {
-            image: cupImg,
-            title: "White coffee",
-            colors: ['black', 'green', 'yellow'],
-            imgClicked: {
-                'black': true,
-                'green': false,
-                'yellow': false,
-            },
-            imgLinked: {
-                'black': cupImg,
-                'green': cupImg1,
-                'yellow': cupImg2
-            }
-        },
-        {
-            image: cupImg,
-            title: "White coffee",
-            colors: ['black', 'green', 'yellow'],
-            imgClicked: {
-                'black': true,
-                'green': false,
-                'yellow': false,
-            },
-            imgLinked: {
-                'black': cupImg,
-                'green': cupImg1,
-                'yellow': cupImg2
-            }
-        },
-        {
-            image: cupImg,
-            title: "White coffee",
-            colors: ['black', 'green', 'yellow'],
-            imgClicked: {
-                'black': true,
-                'green': false,
-                'yellow': false,
-            },
-            imgLinked: {
-                'black': cupImg,
-                'green': cupImg1,
-                'yellow': cupImg2
-            }
-        },
-    ]
+
+    const [coffees, setCoffees] = useState(products);
+    const handleChooseColor = (id, color) => {
+        setCoffees((prev) => {
+            return prev.map((coffee) => {
+                if (coffee.id === id) {
+                    let newImgClicked = {};
+                    //Change all property checkImg false, but color clicked = true
+                    Object.keys(coffee.imgClicked).map((item) => {
+                        coffee.imgClicked[item] = false;
+                        newImgClicked = { ...coffee.imgClicked, [color]: true };
+                        return null;
+                    });
+
+                    return { ...coffee, imgClicked: newImgClicked };
+                } else {
+                    return coffee;
+                }
+            });
+        });
+    };
     return (
         <div className="flex justify-center items-center flex-col overflow-hidden">
 
@@ -62,7 +86,7 @@ const Products = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-32 py-10'>
                 {
-                    products.map((product, i) => (
+                    coffees.map((product, i) => (
                         <div key={i} className='bg-amber-300 shadow-lg flex flex-col justify-center items-center relative p-20 w-96 h-96 rounded-lg'>
                             <div>
                                 <h1 className='text-amber-800 text-4xl font-bold absolute top-0 left-0 py-6 px-3'>{product.title}</h1>
@@ -86,9 +110,23 @@ const Products = () => {
                                         <FaArrowRight />
                                     </button>
                                 </div>
-                                {/* <div className='absolute bottom-0 left-20 mb-2'>
-                                    
-                                </div> */}
+                                <div className='absolute bottom-0 left-20 mb-2'>
+                                    {product.colors.map((color) => (
+                                        <p
+                                            key={color}
+                                            className={` ${product.imgClicked[color] && 'active'}   `}
+                                            style={{
+                                                backgroundColor: color,
+                                                width: 40,
+                                                height: 40,
+                                                borderRadius: '50%',
+                                                marginRight: 10,
+                                                cursor: 'pointer',
+                                            }}
+                                            onClick={() => handleChooseColor(product.id, color)}
+                                        ></p>
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     ))
